4759821756f1d15f4b18510c4f9e93084b7ea538
Sergey Ognev <ognev.s.d@gmail.com>
2024-04-23 19:07:45 +0300

    Add form open and close with bttn

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,35 @@
 import arrow from 'src/images/arrow.svg';
 
+import cn from 'classnames';
+
 import styles from './ArrowButton.module.scss';
+import { FC } from 'react';
 
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+interface ArrowButtonParams {
+	isOpen?: boolean;
+	handleClick?: OnClick;
+}
+
+export const ArrowButton: FC<ArrowButtonParams> = ({ isOpen, handleClick }) => {
+	const openStyle = isOpen ? styles.container_open : null;
+	const arrowOpenStyle = isOpen ? styles.arrow_open : null;
+
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={cn(styles.container, openStyle)}
+			onClick={handleClick}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={cn(styles.arrow, arrowOpenStyle)}
+			/>
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,30 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import cn from 'classnames';
 
 import styles from './ArticleParamsForm.module.scss';
+import { FormEvent, useState } from 'react';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setOpen] = useState(false);
+
+	const openStyle = isOpen ? styles.container_open : null;
+
+	// Handlers
+	const handleClick = () => {
+		setOpen(!isOpen);
+	};
+
+	const handleFormSubmit = (event: FormEvent) => {
+		event.preventDefault();
+		console.warn('Form submitted');
+	};
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={isOpen} handleClick={handleClick} />
+			<aside className={cn(styles.container, openStyle)}>
+				<form className={styles.form} onSubmit={handleFormSubmit}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
