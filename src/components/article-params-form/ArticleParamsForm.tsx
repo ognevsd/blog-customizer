f1ef9d14d34217e0329d2960440e181b994ee913
Sergey Ognev <ognev.s.d@gmail.com>
2024-04-23 19:25:54 +0300

    Add click outside form

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,12 +1,30 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import { Select } from 'components/select';
+import { fontFamilyOptions } from 'src/constants/articleProps';
 import cn from 'classnames';
 
 import styles from './ArticleParamsForm.module.scss';
-import { FormEvent, useState } from 'react';
+import { FormEvent, useEffect, useRef, useState } from 'react';
 
 export const ArticleParamsForm = () => {
 	const [isOpen, setOpen] = useState(false);
+	const formContainer = useRef<HTMLElement>(null);
+
+	useEffect(() => {
+		const handleClickOutsideForm = (event: MouseEvent) => {
+			if (
+				formContainer.current &&
+				!formContainer.current.contains(event.target as Node)
+			) {
+				setOpen(false);
+			}
+		};
+		document.addEventListener('mousedown', handleClickOutsideForm);
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutsideForm);
+		};
+	}, []);
 
 	const openStyle = isOpen ? styles.container_open : null;
 
@@ -23,8 +41,9 @@ export const ArticleParamsForm = () => {
 	return (
 		<>
 			<ArrowButton isOpen={isOpen} handleClick={handleClick} />
-			<aside className={cn(styles.container, openStyle)}>
+			<aside className={cn(styles.container, openStyle)} ref={formContainer}>
 				<form className={styles.form} onSubmit={handleFormSubmit}>
+					<Select selected={null} options={fontFamilyOptions} title='Шрифт' />
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
